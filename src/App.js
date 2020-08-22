09c8f179babf5946723704ae21a15ea5542caa6e
Guilherme-git <guilhermedacatia132@gmail.com>
2020-08-22 00:25:35 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [repositorie, setRepositorie] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositorie(response.data);
+    });
+  }, [repositorie]);
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('/repositories', {
+      title: "Maria",
+      url: "aaaaa",
+      "techs": ["bbbbb","cccc"]
+    });
+    const res = response.data;
+    setRepositorie([...repositorie, res])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
+   
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositorie.map(res =>  
+          <li key={res.id}>
+            {res.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(res.id)}>
+              Remover
+            </button>
+          </li> 
+        )}
+       
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
